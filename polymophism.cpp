78304dd160284dd69bb924f34cc94c99869d5210
msetitech <msetitech@yahoo.com>
2024-12-19 12:53:12 +0300

    Polymophism Concept

---
+++ polymophism.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Employee{
+    private:
+        string name;
+        int id;
+        
+    public:
+    Employee(string empName, int empID):name(empName),id(empID){}
+
+    void displayInformation(){
+        cout << "--- Employee System ---";
+        cout << "Employee Name: " << name << "\nEmployee ID: " << id << endl;
+    }
+};
+
+class Manager : public Employee {
+    private:
+        string bonus;
+    public:
+        Manager(string empName, int empID, string empBonus) : Employee(empName,empID),bonus(empBonus){}
+    
+        void displayInformation()  {
+                cout << "Manager Information\n";
+                Employee::displayInformation() override;
+                cout << "Bonus: " << bonus << endl;
+            }
+};
+
+int main(){
+    Employee *emp1 = new Manager("Ibrahim Mseti", 1234, "Manager");
+    emp1->displayInformation();
+    cout << endl;
+    return 0;
+}
